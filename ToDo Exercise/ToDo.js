6cc0696923abad6f53711a1405314dcbfffc5551
1aid <127182004+1aid@users.noreply.github.com>
2023-03-27 22:28:02 -0400

    Update ToDo.js

--- ToDo Exercise/ToDo.js
+++ ToDo Exercise/ToDo.js
@@ -2,10 +2,30 @@ const form = document.querySelector('#add-item')
 const input = document.querySelector('#item');
 const itemList = document.querySelector('#item-list');
 
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+function renderTodos() {
+  todos.forEach(function(todo) {
+    const newItem = document.createElement('li');
+    newItem.innerText = todo;
+    itemList.appendChild(newItem);
+  });
+}
+
+function saveTodo() {
+  const newItem = document.createElement('li');
+  newItem.innerText = input.value;
+  input.value = '';
+  itemList.appendChild(newItem);
+  todos.push(newItem.innerText);
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
-const newItem = document.createElement('li');
-newItem.innerText = input.value;
-input.value='';
-itemList.appendChild(newItem);
+  e.preventDefault();
+  saveTodo();
+});
+
+window.addEventListener('load', function() {
+  renderTodos();
 });
